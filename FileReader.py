1c590be49620bfc64652bfc6da01cb136a9ea065
Manuel Carvalho <burrowsrd@gmail.com>
2021-02-23 20:26:45 +1100

    Added FileReader.py (1)

--- FileReader.py
+++ FileReader.py
@@ -3,11 +3,17 @@
 
 from tkinter import *
 
+data = ""
+
 def read_file():
     with open("data.csv") as data_file:
         data = data_file.read()
+        text.delete('1.0', END)
+        text.insert(END,data)
 
-        print(data)
+
+        labNum = len(data)
+        kilometer_result_label.config(text=f"{labNum}")
 
 window = Tk()
 window.title("Read data File")
@@ -16,17 +22,18 @@ window.config(padx=20, pady=20)
 miles_input = Entry(width=7)
 miles_input.grid(column=1 , row=0)
 
-miles_label = Label(text="Miles")
+miles_label = Label(text="CSV")
 miles_label.grid(column=2 , row=0 )
 
-is_equal_label = Label(text="is equal to")
-is_equal_label.grid(column=0 , row=1 )
+is_equal_label = Label(text="File Data")
+is_equal_label.config(padx=1, pady=-50)
+is_equal_label.grid(column=0 , row=3 )
 
 kilometer_result_label = Label(text="0")
 kilometer_result_label.grid(column=1 , row=1 )
 
-kilometer_label = Label(text="Km")
-kilometer_label.grid(column=2, row=1 )
+kilometer_label = Label(text="File Count")
+kilometer_label.grid(column=0, row=1 )
 
 calculate_button = Button(text="Read File", command=read_file)
 calculate_button.grid(column=1 , row=2 )
