4de657545f125ed60a6668808558371c09f3d1ce
Manuel Carvalho <burrowsrd@gmail.com>
2021-02-26 18:21:14 +1100

    Added work_timer.py (2)

--- work_timer.py
+++ work_timer.py
@@ -1,2 +1,33 @@
 from tkinter import *
 import math
+
+#Based on Angela Yu's work
+
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+
+
+window = Tk()
+window.title("Work Timer")
+window.config(padx=400, pady=400, bg=YELLOW)
+
+
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
+canvas = Canvas(width=500, height=500, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.grid(column=1, row=1)
+
+
+
+
+
+
+
+window.mainloop()
+
