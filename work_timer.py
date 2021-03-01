3a98e70f8fecdbac7982519c2b99870d020cdbe0
Manuel Carvalho <burrowsrd@gmail.com>
2021-03-02 08:51:02 +1100

    Added work_timer (4)

--- work_timer.py
+++ work_timer.py
@@ -15,8 +15,25 @@ LONG_BREAK_MIN = 20
 reps = 0
 timer = None
 
+
+
 def start_timer():
-    pass
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
 
 
 def count_down(count):
