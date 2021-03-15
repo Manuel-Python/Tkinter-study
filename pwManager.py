857e0908cc84c42c5c0f5c44b401b73d11c37131
Manuel Carvalho <burrowsrd@gmail.com>
2021-03-15 21:04:37 +1100

    Added pwManager.py (3)

--- pwManager.py
+++ pwManager.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
-
+#2
 
 window = Tk()
 window.title("Password Manager")
@@ -20,6 +20,16 @@ email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
+
+#Entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "fizer@gmail.com")
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
 
 
 
