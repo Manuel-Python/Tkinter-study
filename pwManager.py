53973a11d9ad1bab9f26c0cdee566f347965e266
Manuel Carvalho <burrowsrd@gmail.com>
2021-03-20 08:40:53 +1100

    Added pwManager.py (8)

--- pwManager.py
+++ pwManager.py
@@ -55,10 +55,10 @@ canvas.grid(row=0, column=1)
 #Labels
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
-email_label = Label(text="Email/Username:")
+email_label = Label(text="Username:")
 email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
-password_label.grid(row=3, column=0)
+password_label.grid(row=2, column=0)
 
 #Entries
 website_entry = Entry(width=35)
@@ -75,7 +75,7 @@ password_entry.grid(row=3, column=1)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
-add_button.grid(row=4, column=1, columnspan=2)
+add_button.grid(row=4, column=0, columnspan=3)
 
 
 
