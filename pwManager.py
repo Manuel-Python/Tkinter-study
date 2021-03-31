1611e45fe16d4dd655a2c2b86253b3f20735a643
Manuel Carvalho <burrowsrd@gmail.com>
2021-03-31 14:23:51 +1100

    Added find_password (1)

--- pwManager.py
+++ pwManager.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
-import pyperclip
+import json
 #2
 
 def encrypt_password(password):
@@ -69,6 +69,20 @@ def save():
                 password_entry.delete(0, END)
 
 
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
 
 window = Tk()
 window.title("Password Manager")
