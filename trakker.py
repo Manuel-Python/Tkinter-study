fded54177146733dd572ea5b218f352c4d923366
Manuel Carvalho <burrowsrd@gmail.com>
2021-04-06 08:18:10 +1000

    Added trakker.py (iss_position)

--- trakker.py
+++ trakker.py
@@ -6,7 +6,7 @@ iss_lat = 0.0
 iss_long = 0.0
 
 
-def is_iss_overhead():
+def iss_position():
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
     response.raise_for_status()
     data = response.json()
@@ -14,9 +14,10 @@ def is_iss_overhead():
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
 
-    #Your position is within +5 or -5 degrees of the iss position.
-    #if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
-     #   return True
-    return iss_latitude
+    iss_loc = (iss_latitude, iss_longitude)
 
-print(is_iss_overhead())
+
+    return iss_loc
+
+for n in range(20):
+    print(iss_position())
