0bd4222cbaf5c8bb611eaccfa862de37c03500e2
Manuel Carvalho <burrowsrd@gmail.com>
2021-04-08 08:39:28 +1000

    Added tupple list

--- trakker.py
+++ trakker.py
@@ -7,6 +7,7 @@ MY_LONG = -0.127758 # Your longitude
 iss_lat = 0.0
 iss_long = 0.0
 
+positionISS = []
 
 def iss_position():
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
@@ -21,49 +22,6 @@ def iss_position():
 
     return iss_loc
 
-for n in range(20):
-    print(iss_position())
-
-
-# import Style class from pygal.style
-from pygal.style import Style
-
-# create a Style object
-custom_style = Style(colors=('#FF0000', '#0000FF',
-                             '#00FF00', '#000000',
-                             '#FFD700'))
-
-# create a world map,
-# Style class is used for using
-# the custom colours in the map,
-worldmap = pygal.maps.world.World(style
-                                  =custom_style)
-
-# set the title of the map
-worldmap.title = 'Some Countries Starting from Specific Letters'
-
-# hex code of colours are used
-# for every .add() called
-worldmap.add('"E" Countries',
-             ['ec', 'ee', 'eg', 'eh',
-              'er', 'es', 'et'])
-
-worldmap.add('"F" Countries',
-             ['fr', 'fi'])
-
-worldmap.add('"P" Countries',
-             ['pa', 'pe', 'pg', 'ph', 'pk',
-              'pl', 'pr', 'ps', 'pt', 'py'])
-
-worldmap.add('"Z" Countries',
-             ['zm', 'zw'])
-
-worldmap.add('"A" Countries',
-             ['ad', 'ae', 'af', 'al', 'am', 'ao',
-              'aq', 'ar', 'at', 'au', 'az'],
-             color='black')
-
-# save into the file
-worldmap.render_to_file('abc.svg')
-
-print("Success")
+for n in range(200):
+    positionISS.append(iss_position())
+    print(positionISS)
