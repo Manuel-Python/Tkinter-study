852f585ffa40445900bf0b24e9cb3fb45cf0e219
Manuel Carvalho <burrowsrd@gmail.com>
2021-05-23 08:11:00 +1000

    Added asciiCode.py (2)

--- asciiCode.py
+++ asciiCode.py
@@ -4,39 +4,43 @@ import os
 import requests
 
 
-nums = []
+#nums = []
 
 
+def open_file():
+	with open('text.txt', 'r') as line:
+		nums = []
+		file_text = line.read()
+		for char in file_text:
+			num = str(ord(char))
+			nums.append(num)
+		return nums
 
-with open('text.txt', 'r') as line:
-	file_text = line.read()
-	for char in file_text:
-		num = str(ord(char))
-		nums.append(num)
+def write_file(nums):
+	with open('table.txt', 'w') as file:
+		count = 0
+		line = "	db.b "
+		for num in nums:
+			line += num + ","
+			count += 1
+			if count > 8:
+				l = len(line)
+				line = line[:l-1]
+				file.write(line + "\n")
+				line = "	db.b "
+				count = 0
 
-
+def process_file():
+	nums = open_file()
 	print(nums)
-
-
-with open('table.txt', 'w') as file:
-	count = 0
-	line = "	db.b "
-	for num in nums:
-		line += num + ","
-		count += 1
-		if count > 8:
-			l = len(line)
-			line = line[:l-1]
-			file.write(line + "\n")
-			line = "	db.b "
-			count = 0
-
-	print(line)
-	# file.write(nums)
+	write_file(nums)
 
 window = Tk()
 window.title("ASCII Encoder")
 window.config(padx=20, pady=20)
 
+
+read_button = Button(text="Read File", command=process_file)
+read_button.grid(row=2, column=1)
 
 window.mainloop()
